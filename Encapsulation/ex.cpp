2d616753e3de31e5425aea1a01d75b508547dfe5
Gyan Prakash Rai <shanuraig@gmail.com>
2025-07-27 01:33:20 +0530

    oops concept

---
+++ Encapsulation/ex.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+class student{
+string name;
+int age;
+int weight;
+public:
+int getage(){
+    return this->age;
+}
+};
+//this is used to hide the information  ,for increasing security , Code reusability and unit testing.
---
+++ Inheritence/eg.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+class human{
+string name;
+int age;
+float height;
+double weight;
+};
+class male:public human{
+string name;
+};
